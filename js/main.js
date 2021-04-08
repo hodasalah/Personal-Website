3b987910c20208fb00d17f03ff9f106f02363dad
hodasalah <hodasalah35@gmail.com>
2021-04-08 02:40:12 +0200

    fixed

--- js/main.js
+++ js/main.js
@@ -1,111 +1,72 @@
-/* ========== JS File ==========
-    Template Name: Personal Template
-    Author: Hoda salah
-    Version: 1.0
-    Copyright 2018
-========== HTML File ==========
-
-JS FILE INDEX
-=============
-
-00- Fade Out preloader
-01- Img Hover 3d
-02- Show And Hide Sections
-03- Animated Lines 
-04-  Jqyery Show & Hide Animation Loaded
-05- Auto Type Setup
-06- Testimonials Slider
-07 - Service Slider
-08 - Clients Slider
-09 - portfolio list 
-10 - progress bar
-==================================================== */
-
 $(document).ready(function () {
-
-    'use strict';
-
+    "use strict";
 
     // 01 - Fade Out preloader
     $('.preloader .line').delay(1000).fadeOut(500);
     $('.preloader').delay(1500).fadeOut(500);
 
-
-
     // Img Hover 3d
     $(".move").hover3d({
         selector: ".movie__card",
         shine: false,
-        sensitivity: 50
+        sensitivity: 50,
     });
 
-
-    
     // Show And Hide Sections
-    $('.show-about-content, .pe-7s-mouse').on('click', function () {
-
-        $('.about-me').addClass('show-about');
-
+    $(".show-about-content, .pe-7s-mouse").on("click", function () {
+        $(".about-me").addClass("show-about");
     });
 
-    $('.about-me i.la-close').on('click', function () {
-
-        $('.about-me').removeClass('show-about');
-
+    $(".about-me i.la-close").on("click", function () {
+        $(".about-me").removeClass("show-about");
     });
 
-
-    // Animated Lines 
-    $('.section-button').on('click', function (e) {
-        var lines_grid = $('.lines-grid');
+    // Animated Lines
+    $(".section-button").on("click", function (e) {
+        var lines_grid = $(".lines-grid");
         e.preventDefault();
-        lines_grid.removeClass('loaded');
+        lines_grid.removeClass("loaded");
 
-        var section = $($(this).data('section'));
+        var section = $($(this).data("section"));
 
-        section.addClass('show');
+        section.addClass("show");
 
         setTimeout(function () {
-            lines_grid.addClass('loaded');
+            lines_grid.addClass("loaded");
         }, 1000);
     });
-
 
     // Jqyery Show & Hide Animation Loaded
-    $('.section-close i, .section-close').on('click', function (e) {
-
-        var lines_grid = $('.lines-grid');
+    $(".section-close i, .section-close").on("click", function (e) {
+        var lines_grid = $(".lines-grid");
         e.preventDefault();
 
-        lines_grid.removeClass('loaded');
+        lines_grid.removeClass("loaded");
 
-        var section = $($(this).data('section'));
+        var section = $($(this).data("section"));
 
-        section.removeClass('show');
+        section.removeClass("show");
 
         setTimeout(function () {
-            lines_grid.addClass('loaded');
+            lines_grid.addClass("loaded");
         }, 1000);
-
-
     });
-
-
-
 
     //  Auto Type Setup
 
-    $('.short-info .typed').typed({
-        strings: ["<span>Freelancer.</span>&nbsp;", "<span>Ui & Ux Designer.</span>&nbsp;", "<span>Front End Web developer.</span>"],
+    $(".short-info .typed").typed({
+        strings: [
+            "<span>Freelancer.</span>&nbsp;",
+            "<span>Ui & Ux Designer.</span>&nbsp;",
+            "<span>Front End Web developer.</span>",
+        ],
         loop: true,
         startDelay: 1000,
-        backDelay: 2000
+        backDelay: 2000,
     });
 
-
-
     // Testimonials Slider
-    $('.testimonials-carousel').owlCarousel({
+    $(".testimonials-carousel").owlCarousel({
         loop: false,
         items: 1,
         autoplay: true,
@@ -113,52 +74,52 @@ $(document).ready(function () {
         autoplayHoverPause: true,
         nav: true,
         dots: false,
-        navText: ["<i class='pe-7s-angle-left'></i>",
-                     "<i class='pe-7s-angle-right'></i>"]
-
+        navText: [
+            "<i class='pe-7s-angle-left'></i>",
+            "<i class='pe-7s-angle-right'></i>",
+        ],
     });
     // Service Slider
-    $('.service-carousel').owlCarousel({
+    $(".service-carousel").owlCarousel({
         loop: true,
         items: 1,
         nav: true,
         dots: false,
-        navText: ["<i class='pe-7s-angle-left'></i>",
-                     "<i class='pe-7s-angle-right'></i>"],
+        navText: [
+            "<i class='pe-7s-angle-left'></i>",
+            "<i class='pe-7s-angle-right'></i>",
+        ],
         autoplay: true,
         autoplayTimeout: 3000,
-        autoplayHoverPause: true
-
+        autoplayHoverPause: true,
     });
-    
 
     //  Clients Slider
-    $('.clients-carousel').owlCarousel({
+    $(".clients-carousel").owlCarousel({
         loop: true,
         items: 3,
         nav: true,
         dots: false,
         responsiveClass: true,
-        navText: ["<i class='pe-7s-angle-left'></i>",
-                     "<i class='pe-7s-angle-right'></i>"],
+        navText: [
+            "<i class='pe-7s-angle-left'></i>",
+            "<i class='pe-7s-angle-right'></i>",
+        ],
         responsive: {
             0: {
-                items: 1
+                items: 1,
             },
             480: {
-                items: 1
+                items: 1,
             },
             992: {
-                items: 3
+                items: 3,
             },
             1200: {
-                items: 3
-            }
-        }
+                items: 3,
+            },
+        },
     });
-
-
-    
 
     // portfolio list
     var portPart = $(".my-portfolio .row .mix");
@@ -166,36 +127,25 @@ $(document).ready(function () {
         $(this).hoverdir();
     });
 
-    $('.grid').mixItUp();
+    $(".grid").mixItUp();
 
-
-    
-    
-    
     // progress bar
     function animateProgressBar() {
-        $('.skill-box .progress-line > span').each(function () {
-            var percent = $(this).data('percent');
+        $(".skill-box .progress-line > span").each(function () {
+            var percent = $(this).data("percent");
             $(this).css({
-                width: percent + '%',
-                transition: 'width 1.5s linear'
+                width: percent + "%",
+                transition: "width 1.5s linear",
             });
         });
     }
 
-    $(window).on('scroll', function () {
-        if ($(window).scrollTop() > $('.my-resume').offset().top - 60) {
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > $(".my-resume").offset().top - 60) {
             animateProgressBar();
         }
     });
-    if ($(window).scrollTop() > $('.my-resume').offset().top - 60) {
+    if ($(window).scrollTop() > $(".my-resume").offset().top - 60) {
         animateProgressBar();
     }
-
-    
-    
 });
-
-
-
-
